0854d1d348378b96f5c330d7efd27bf7946953dd
Alejandro Alberto Gavira García <alex.gavira@gmail.com>
2023-11-29 13:15:56 +0100

    Initial Commit

---
+++ infoH4x0r/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  infoH4x0r
+//
+//  Created by Alejandro Alberto Gavira García on 29/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ infoH4x0r/infoH4x0rApp.swift
@@ -0,0 +1,17 @@
+//
+//  infoH4x0rApp.swift
+//  infoH4x0r
+//
+//  Created by Alejandro Alberto Gavira García on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct infoH4x0rApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
